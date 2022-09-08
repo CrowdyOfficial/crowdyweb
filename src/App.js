a1aae733e3793fb446ad109051ca543286affa71
chapsaldduck <koala031532@gmail.com>
2022-09-08 16:03:34 +0900

    modified: App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from "./logo.svg";
 import "./style/App.scss";
 
 function App() {
-  return <div className="App">hello crowdy!</div>;
+  return (
+    <div className="App">
+      <div className="background">hello crowdy</div>
+    </div>
+  );
 }
 
 export default App;
