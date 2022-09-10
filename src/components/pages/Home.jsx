e3e36c51655dd204114d75f5a4f64cc7d4653228
chapsaldduck <koala031532@gmail.com>
2022-09-11 06:40:07 +0900

    modified: lereve magazine

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div className="App">Home</div>;
+  return (
+    <div className="App">
+      Home
+      <br />
+      <Link to={"/lereve"}>Lereve</Link>
+    </div>
+  );
 };
 
 export default Home;
