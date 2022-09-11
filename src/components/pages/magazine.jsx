7894696f63e96bf42d4ceee4218c29a0e1adf796
chapsaldduck <koala031532@gmail.com>
2022-09-11 12:00:51 +0900

    modified: Lereve

--- src/components/pages/magazine.jsx
+++ src/components/pages/magazine.jsx
@@ -153,17 +153,20 @@ const LeReve = () => {
           <span>영업시간: 매일 10:00 ~ 23:59</span>
           <br />
           <span className="links">
-            외부링크:&nbsp;&nbsp;&nbsp;
-            <a href="https://map.naver.com/v5/entry/place/1267521105?c=14143706.329624798,4468556.865391634,13,0,0,0,dh&placePath=%2Fhome&entry=plt">
-              <img src="assets/icon/naver.png" alt="" />
-              &nbsp;&nbsp;네이버
-            </a>
+            <span>외부링크:</span>
             <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <a href="https://www.instagram.com/cafe_lereve/">
-              <img src="assets/icon/insta.png" alt="" />
-              &nbsp;&nbsp;인스타
-            </a>
+            <br />
+            <div>
+              <a href="https://map.naver.com/v5/entry/place/1267521105?c=14143706.329624798,4468556.865391634,13,0,0,0,dh&placePath=%2Fhome&entry=plt">
+                <img src="assets/icon/naver.png" alt="" />
+                &nbsp;&nbsp;네이버
+              </a>
+              <br />
+              <a href="https://www.instagram.com/cafe_lereve/">
+                <img src="assets/icon/insta.png" alt="" />
+                &nbsp;&nbsp;인스타
+              </a>
+            </div>
           </span>
         </div>
       </div>
