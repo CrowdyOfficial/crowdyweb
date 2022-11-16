a2e2ce35f4821f8f3810682d50afb5419b7e8d7e
chapsaldduck <koala031532@gmail.com>
2022-11-17 00:25:19 +0900

    modified: thumbnails png -> webp

--- src/store.js
+++ src/store.js
@@ -14,39 +14,39 @@ const useStore = create(() => ({
     list: [
       {
         to: "/jamjam",
-        thumbnail: "./assets/magazinePages/jamjam/thumbnail.png",
+        thumbnail: "./assets/magazinePages/jamjam/thumbnail.webp",
         cafeName: "잼잼",
         cafeDesc: "흑임자로 받은 사랑을 서비스로 보답하다",
       },
       {
         to: "/pyojeong",
-        thumbnail: "./assets/magazinePages/pyojeong/thumbnail.png",
+        thumbnail: "./assets/magazinePages/pyojeong/thumbnail.webp",
         cafeName: "표정",
         cafeDesc: "작은 공간에서 구겔호프가 주는 특별함",
       },
       {
         to: "/geeksyeonnam",
-        thumbnail: "./assets/magazinePages/geeksyeonnam/thumbnail.png",
+        thumbnail: "./assets/magazinePages/geeksyeonnam/thumbnail.webp",
         cafeName: "긱스 연남점",
         cafeDesc: "다양한 종류의 원두에서 나의 커피 취향을 찾다",
       },
       {
         to: "/atthemoment",
         thumbnail:
-          "./assets/magazinePages/atthemoment/atthemoment_thumbnail.png",
+          "./assets/magazinePages/atthemoment/atthemoment_thumbnail.webp",
         cafeName: "앳더모먼트",
         cafeDesc: "계절의 향과 글루텐프리 바스크 치즈케이크",
       },
       {
         to: "/tenderly",
         thumbnail:
-          "./assets/magazinePages/tenderlycoffee/tenderly_thumbnail.png",
+          "./assets/magazinePages/tenderlycoffee/tenderly_thumbnail.webp",
         cafeName: "텐더리커피앤디저트",
         cafeDesc: "따뜻하고 아늑한 공간의 여유로움",
       },
       {
         to: "/yenani",
-        thumbnail: "./assets/magazinePages/yenani/yenani_thumbnail.png",
+        thumbnail: "./assets/magazinePages/yenani/yenani_thumbnail.webp",
         cafeName: "예나니",
         cafeDesc: "끝남동의 매력적인 카페, 아기자기한 감성",
       },
